06edc26a4b3d7579f55ea728f6d4128bb8b968cd
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-06-06 17:35:11 +0100

    feat(endpoint-micropub): derive microformat properties

--- filters/date.js
+++ filters/date.js
@@ -1,19 +1,21 @@
 import luxon from 'luxon';
 
+const {DateTime} = luxon;
+
 /**
- * Format a date.
+ * Format a date
  *
  * @param {string} string ISO 8601 date
  * @param {string} format Tokenised date format
- * @param {string} locale Locale
+ * @param {object} locale Locale to use for formatting datetime
+ * @param {object} zone Timezone offset
  * @returns {string} Formatted date
  */
-export const date = (string, format, locale = 'en-GB') => {
-  const {DateTime} = luxon;
+export const date = (string, format, locale = 'en-GB', zone = 'utc') => {
   const date = (string === 'now') ? DateTime.local() : string;
   const datetime = DateTime.fromISO(date, {
     locale,
-    zone: 'utc'
+    zone
   }).toFormat(format);
   return datetime;
 };
